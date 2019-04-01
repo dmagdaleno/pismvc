bc57e451d93d8977c966b0c700153434aed136a9
dmagdaleno <diegommagdaleno@gmail.com>
2019-04-01 19:06:52 -0300

    utiliza log4j

--- src/br/com/fiap/imposto/model/Pis.java
+++ src/br/com/fiap/imposto/model/Pis.java
@@ -1,15 +1,26 @@
 package br.com.fiap.imposto.model;
 
-import br.com.fiap.imposto.util.AliquotaSingleton;
-
 import java.util.Observable;
 
-// O Model é um Observable
-// A Classe Pis da camada Model não possui referência a View ou ao // Controller
-// Ao implementar Imposto, teremos maior flexibilidade no Controller
-public class Pis extends Observable implements Imposto{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import br.com.fiap.imposto.util.AliquotaSingleton;
 
-    final float ALIQUOTA = Float.parseFloat(AliquotaSingleton.getInstance().getProperty("aliquotaPis"));
+
+
+// O Model é um Observable
+// A Classe Pis da camada Model não possui referência a View ou ao Controller
+public class Pis extends Observable implements Imposto {
+
+    // LogManager retorna uma instância de Logger, passando o nome
+    // da classe que será logada como parâmetro
+    private static final Logger LOGGER
+            = LogManager.getLogger(Pis.class.getName());
+
+
+    // Recebe do Singleton o valor da propriedade aliquotaPis
+    final float ALIQUOTA = Float
+            .parseFloat(AliquotaSingleton.getInstance().getProperty("aliquotaPis"));
     float valorDoPis = 0;
 
     public Pis() {
@@ -25,10 +36,18 @@ public class Pis extends Observable implements Imposto{
     }
 
     public void calcularImposto(float valor) {
+
         valorDoPis = valor * ALIQUOTA;
+
+        // Grava o log, conforme parametrizado
+        LOGGER.info("Valor: " + valor
+                + " Alíquota: " + ALIQUOTA
+                + " Valor do Pis: " + valorDoPis);
+
         // setChanged Altera o estado interno do objeto
         // para true, pois houve alteração no estado valorDoPis
         setChanged();
+
         // Os observadores devem ser notificados
         // Envia o valor do PIS como parte da mensagem de
         // notificação para a View que é um Observer
@@ -40,4 +59,5 @@ public class Pis extends Observable implements Imposto{
         return "Pis [ALIQUOTA=" + ALIQUOTA
                 + ", valorDoPis=" + valorDoPis + "]";
     }
+
 }
